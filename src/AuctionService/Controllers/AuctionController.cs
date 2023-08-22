91faac0110427f0da8f226ec502c7e5d8d55088a
selimozcan <selimozcan17@gmail.com>
2023-08-23 00:29:09 +0300

    Initial Commit

---
+++ src/AuctionService/Controllers/AuctionController.cs
@@ -0,0 +1,45 @@
+﻿using AuctionService.Data;
+using AuctionService.DTOs;
+using AuctionService.Entities;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService;
+
+[ApiController]
+[Route("api/auctions")]
+public class AuctionController : ControllerBase
+{
+    private readonly AuctionDbContext _context;
+    private readonly IMapper _mapper;
+
+    public AuctionController(AuctionDbContext context, IMapper mapper)
+    {
+        this._context = context;
+        this._mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions()
+    {
+        var auctions = await _context.Auctions
+            .Include(a => a.Item)
+            .OrderBy(a => a.Item.Make)
+            .ToListAsync();
+
+        return _mapper.Map<List<AuctionDto>>(auctions);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AuctionDto>> GetAuctionById(Guid id)
+    {
+        var auction = await _context.Auctions
+            .Include(a => a.Item)
+            .FirstOrDefaultAsync(a => a.Id == id);
+
+        if (auction == null) return NotFound();
+
+        return _mapper.Map<AuctionDto>(auction);
+    }
+}
