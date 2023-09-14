de669fe32149406d4fe98775ecbbf26fd3c06f53
selimozcan <selimozcan17@gmail.com>
2023-09-14 21:40:33 +0300

    Added search service

---
+++ src/SearchService/Program.cs
@@ -0,0 +1,31 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddHttpClient<AuctionSvcHttpClient>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+try
+{
+    await DbInitializer.InitDb(app);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+app.Run();
---
+++ src/SearchService/Services/AuctionSvcHttpClient.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Entities;
+
+namespace SearchService;
+
+public class AuctionSvcHttpClient
+{
+    private readonly HttpClient _httpClient;
+    private readonly IConfiguration _configuration;
+
+    public AuctionSvcHttpClient(HttpClient httpClient, IConfiguration configuration)
+    {
+        this._httpClient = httpClient;
+        this._configuration = configuration;
+    }
+
+    public async Task<List<Item>> GetItemsForSearchDb()
+    {
+        var lastUpdated = await DB.Find<Item, string>()
+            .Sort(a => a.Descending(a => a.UpdatedAt))
+            .Project(a => a.UpdatedAt.ToString())
+            .ExecuteFirstAsync();
+
+        return await _httpClient.GetFromJsonAsync<List<Item>>(_configuration["AuctionSvcHttpClient"] + "/api/auctions?date=" + lastUpdated);
+    }
+}
